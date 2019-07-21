2256c675c2f98403931431133f19bf2344759eed
nosson <48989814+nossonCotlar@users.noreply.github.com>
2019-07-21 18:09:59 -0500

    Initial commit

---
+++ ref.ino
@@ -0,0 +1,33 @@
+//feed 
+/*
+const int reference() {
+for(int load = 0; load < 400; load++) {
+  frame[load] = pgm_read_word( &ref[load] );
+}
+scan(1);
+}
+*/
+
+//reference!!
+/*const PROGMEM int standard[] = {
+//0,1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9
+  O,O,O,O,O,O,O,O,O,O,O,O,O,O,O,O,O,O,O,O,
+  O,O,O,O,O,O,O,O,O,O,O,O,O,O,O,O,O,O,O,O,
+  O,O,O,O,O,O,O,O,O,O,O,O,O,O,O,O,O,O,O,O,
+  O,O,O,O,O,O,O,O,O,O,O,O,O,O,O,O,O,O,O,O,
+  O,O,O,O,O,O,O,O,O,O,O,O,O,O,O,O,O,O,O,O,
+  O,O,O,O,O,O,O,O,O,O,O,O,O,O,O,O,O,O,O,O,
+  O,O,O,O,O,O,O,O,O,O,O,O,O,O,O,O,O,O,O,O,
+  O,O,O,O,O,O,O,O,O,O,O,O,O,O,O,O,O,O,O,O,
+  O,O,O,O,O,O,O,O,O,O,O,O,O,O,O,O,O,O,O,O,
+  O,O,O,O,O,O,O,O,O,O,O,O,O,O,O,O,O,O,O,O,
+  O,O,O,O,O,O,O,O,O,O,O,O,O,O,O,O,O,O,O,O,
+  O,O,O,O,O,O,O,O,O,O,O,O,O,O,O,O,O,O,O,O,
+  O,O,O,O,O,O,O,O,O,O,O,O,O,O,O,O,O,O,O,O,
+  O,O,O,O,O,O,O,O,O,O,O,O,O,O,O,O,O,O,O,O,
+  O,O,O,O,O,O,O,O,O,O,O,O,O,O,O,O,O,O,O,O,
+  O,O,O,O,O,O,O,O,O,O,O,O,O,O,O,O,O,O,O,O,
+  O,O,O,O,O,O,O,O,O,O,O,O,O,O,O,O,O,O,O,O,
+  O,O,O,O,O,O,O,O,O,O,O,O,O,O,O,O,O,O,O,O,
+  O,O,O,O,O,O,O,O,O,O,O,O,O,O,O,O,O,O,O,O,
+  O,O,O,O,O,O,O,O,O,O,O,O,O,O,O,O,O,O,O,O };*/
